e99c0a9dfcef8d13f5a67ff3acd2d28d547c98f4
Don Huan <x.mister@bk.ru>
2013-11-03 23:11:01 +0400

    Добавлено считывание суммы и остатка из текстового сообщения.

--- src/com/donhuan/SmshubAndroid/MyActivity.java
+++ src/com/donhuan/SmshubAndroid/MyActivity.java
@@ -6,7 +6,7 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MyActivity extends Activity {
-    //1
+    public static final String EXAMPLE_TEST = "ЗСКБ 9876 11янв 13:02 оплата 500р, остаток 5200.50р";
 
     TextView tvHello;
     /**
@@ -20,8 +20,33 @@ public class MyActivity extends Activity {
         tvHello = (TextView) findViewById(R.id.textView1);
     }
 
-    public void onClick(View v){
-        tvHello.setText("Hello Kitty!");
+    public void onClick(View v) {
+        String[] splitString = (EXAMPLE_TEST.split("\\s+"));
+        String oldWord = "";
+        String titleText = "";
+        for (String word : splitString) {
+            if (oldWord.equals("оплата")) {
+                titleText = titleText + "Сумма оплаты: " + readAmounts(word) + "\n";
+            }
+            if (oldWord.equals("остаток")) {
+                titleText = titleText + "Сумма остатка: " + readAmounts(word) + "\n";
+            }
+            tvHello.setText(titleText);
+            oldWord = word;
+        }
+
+    }
+
+    private double readAmounts(String str) {
+        double mount = 0;
+        for (int i = str.length(); i > 0; i--) {
+            try {
+                mount = Double.parseDouble(str.substring(0, i));
+                return mount;
+            } catch (NumberFormatException ignore) {
+            }
+        }
+        return mount;
     }
 
 }
