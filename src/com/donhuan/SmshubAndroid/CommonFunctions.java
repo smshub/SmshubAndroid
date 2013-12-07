3b81e0edd8d84cad80f289404fe5c786f5bef58c
Roman <pom6uk@gmail.com>
2013-12-07 16:53:42 +0400

    Функция записи в базу данных клвсса CommonFunctions.

---
+++ src/com/donhuan/SmshubAndroid/CommonFunctions.java
@@ -0,0 +1,12 @@
+package com.donhuan.SmshubAndroid;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Роман
+ * Date: 07.12.13
+ * Time: 16:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class CommonFunctions {
+
+}
